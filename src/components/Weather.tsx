5fe24927da35fd2fda7927efc5ef1328548a3bab
Justin van der Kwaak <justinkwaak@hotmail.com>
2024-01-23 16:17:56 +0100

    Fixed error

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,5 +1,5 @@
 import { useState, ChangeEvent, FormEvent } from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Search from "./Search";
 import Forecast from "./Forecast";
 import { fetchWeatherData, fetchForecastData } from "../utils/WeatherApi";
@@ -11,8 +11,10 @@ const Weather: React.FC = () => {
   const [animationKey, setAnimationKey] = useState(0);
 
   // Hier wordt de data gefetched
-  const { data: weatherData } = fetchWeatherData(location);
-  const { data: forecastData } = fetchForecastData(location);
+  const { data: weatherData, isError: weatherError } =
+    fetchWeatherData(location);
+  const { data: forecastData, isError: forecastError } =
+    fetchForecastData(location);
 
   console.log(weatherData, forecastData);
 
@@ -33,63 +35,78 @@ const Weather: React.FC = () => {
         handleInputChange={handleInputChange}
         handleSubmit={handleSubmit}
       />
-      {weatherData ? (
-        <motion.div key={animationKey} className="weather__output">
-          <motion.h1
-            initial={{ opacity: 0, y: -30 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, ease: "easeIn" }}
-            className="weather__output-location"
-          >
-            {weatherData.name}
-          </motion.h1>
-          <div className="weather__output-temp">
-            <motion.img
-              initial={{ opacity: 0, y: -30 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: 0.2, ease: "easeIn" }}
-              className="weather__output-icon"
-              src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`}
-            />
+      {weatherError ? (
+        <p>Deze stad bestaat niet! Probeer een andere stad</p>
+      ) : (
+        weatherData && (
+          <motion.div key={animationKey} className="weather__output">
             <motion.h1
               initial={{ opacity: 0, y: -30 }}
               animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5, delay: 0.2, ease: "easeIn" }}
+              transition={{ duration: 0.5, ease: "easeIn" }}
+              className="weather__output-location"
             >
-              {weatherData.main.temp.toFixed(0)}
-              <span>°C</span>
+              {weatherData.name}
             </motion.h1>
-          </div>
+            <div className="weather__output-temp">
+              <motion.img
+                initial={{ opacity: 0, y: -30 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.2, ease: "easeIn" }}
+                className="weather__output-icon"
+                src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`}
+              />
+              <motion.h1
+                initial={{ opacity: 0, y: -30 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.5, delay: 0.2, ease: "easeIn" }}
+              >
+                {weatherData.main.temp.toFixed(0)}
+                <span>°C</span>
+              </motion.h1>
+            </div>
 
-          <motion.p
-            className="weather__output-description"
-            initial={{ opacity: 0, y: -30 }}
+            <motion.p
+              className="weather__output-description"
+              initial={{ opacity: 0, y: -30 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.3, ease: "easeIn" }}
+            >
+              {weatherData.weather[0].description}
+            </motion.p>
+
+            <motion.p
+              className="weather__output-wind"
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.4, ease: "easeIn" }}
+            >
+              Windsnelheid: {(weatherData.wind.speed * 3.6).toFixed(1)} km/h
+            </motion.p>
+
+            <motion.hr
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.6, ease: "easeIn" }}
+            />
+
+            <Forecast forecastData={forecastData} />
+          </motion.div>
+        )
+      )}{" "}
+      <AnimatePresence>
+        {!weatherError && !weatherData && (
+          <motion.h1
+            className="weather__retrieve"
+            initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.3, ease: "easeIn" }}
+            exit={{ opacity: 0, y: 10 }}
+            transition={{ duration: 0.5, ease: "easeIn" }}
           >
-            {weatherData.weather[0].description}
-          </motion.p>
-
-          <motion.p
-            className="weather__output-wind"
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.4, ease: "easeIn" }}
-          >
-            Windsnelheid: {(weatherData.wind.speed * 3.6).toFixed(1)} km/h
-          </motion.p>
-
-          <motion.hr
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.5, delay: 0.6, ease: "easeIn" }}
-          />
-
-          <Forecast forecastData={forecastData} />
-        </motion.div>
-      ) : (
-        <h1>Weerinformatie ophalen</h1>
-      )}
+            Weerinformatie ophalen
+          </motion.h1>
+        )}
+      </AnimatePresence>
     </section>
   );
 };
--- src/utils/WeatherApi.tsx
+++ src/utils/WeatherApi.tsx
@@ -1,64 +1,47 @@
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 
-// Hier heb ik de interface voor de current weather
-export interface CurrentWeatherData {
-  name: string;
-  main: {
-    temp: number;
-  };
-  weather: {
-    description: string;
-    icon: string;
-  }[];
-  wind: {
-    speed: number;
-  };
-}
-
-// Hier heb ik de interface voor de voorspellingen
-export interface ForecastWeatherData {
-  list: {
-    dt_txt: string;
-    weather: {
-      icon: string;
-      description: string;
-    }[];
-    main: {
-      temp: number;
-    };
-  }[];
-}
-
 // Dit is de functie voor het ophalen van de current weather
 export const fetchWeatherData = (location: string) => {
   const { data, isError } = useQuery({
     queryKey: ["weather", location],
     queryFn: async () => {
-      const response = await axios.get(
-        `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&lang=nl&limit=5&appid=${
-          import.meta.env.VITE_API_KEY
-        }`
-      );
-      return response.data;
+      try {
+        const response = await axios.get(
+          `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&lang=nl&limit=5&appid=${
+            import.meta.env.VITE_API_KEY
+          }`
+        );
+        return response.data;
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+        // You can handle the error here, and optionally rethrow it if you want to propagate it further.
+        throw isError;
+      }
     },
   });
 
-  return { data };
+  return { data, isError };
 };
 
 export const fetchForecastData = (location: string) => {
   const { data, isError } = useQuery({
     queryKey: ["forecast", location],
     queryFn: async () => {
-      const response = await axios.get(
-        `http://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&lang=nl&appid=${
-          import.meta.env.VITE_API_KEY
-        }`
-      );
-      return response.data;
+      try {
+        const response = await axios.get(
+          `http://api.openweathermap.org/data/2.5/forecast?q=${location}&units=metric&lang=nl&appid=${
+            import.meta.env.VITE_API_KEY
+          }`
+        );
+        return response.data;
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+        // You can handle the error here, and optionally rethrow it if you want to propagate it further.
+        throw isError;
+      }
     },
   });
 
-  return { data };
+  return { data, isError };
 };
